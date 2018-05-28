9390f07cd3e2c2308141407fea32a6e74d1c9517
Ronny.Wendler <phracker91@gmail.com>
2018-05-28 11:28:03 +0200

    Anlegen der HubHeader Klasse...

---
+++ src/PIZZA.Hub.Core/HubHeader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PIZZA.Hub.Core
+{
+    class HubHeader
+    {
+        public byte[] ProtocollName;
+        public byte ProtocollVersion;
+        public byte[] PacketType;
+    }
+}
