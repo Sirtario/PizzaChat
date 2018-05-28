280a8f6854015af4c395da06733933f87163b036
Ronny.Wendler <phracker91@gmail.com>
2018-05-28 12:13:34 +0200

    HubHeader Ini...

--- src/PIZZA.Hub.Core/HubHeader.cs
+++ src/PIZZA.Hub.Core/HubHeader.cs
@@ -1,13 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using PIZZA.Core;
 
 namespace PIZZA.Hub.Core
 {
     class HubHeader
     {
-        public byte[] ProtocollName;
-        public byte ProtocollVersion;
-        public byte[] PacketType;
+        private byte[] _protocollName;
+        private byte _protocollVersion;
+
+        public HubPacketTypes PacketType { get; set; }
+
+        public PIZZAInt3 PayloadLength { get; set; }
+
+        public void SetProtocollVersion(byte Version)
+        {
+            _protocollVersion = Version;
+        }
+
+        public byte GetProtocollVersion()
+        {
+            return _protocollVersion;
+        }
+
     }
 }
