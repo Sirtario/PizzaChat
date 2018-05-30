7db2cdae3155069b4f280fa84afdb39e3cb3a1ab
Ronny.Wendler <phracker91@gmail.com>
2018-05-30 09:46:21 +0200

    Aufarbeiten des Verlustes Arbeit an den PayLoads

---
+++ src/PIZZA.Hub.Core/PayLoads/HubHostlistDatPayLoad.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using PIZZA.Core;
+
+namespace PIZZA.Hub.Core.PayLoads
+{
+    class HubHostlistDatPayLoad : HubPayLoad
+    {
+        private List<HubHostInfo> _hosts;
+
+        private HubHostlistDatPayLoad(byte[] bytes)
+        {
+            List<byte> tmp = bytes.ToList();
+
+            while (tmp.Count != 0)
+            {
+                HubHostInfo _current = new HubHostInfo();
+
+                _current.Hostname = PIZZAString.FromBytes(tmp.ToArray());
+
+                tmp.RemoveRange(0, 2);
+
+                _current.Friendlyname = PIZZAString.FromBytes(tmp.ToArray());
+
+                tmp.RemoveRange(0, 2);
+
+                _current.Description = PIZZAString.FromBytes(tmp.ToArray());
+
+                tmp.RemoveRange(0, 2);
+
+                _current.RequiresPassword = tmp[0];
+
+                tmp.RemoveAt(0);
+
+                _hosts.Add(_current);
+            }
+
+        }
+
+        public HubHostlistDatPayLoad FromBytes(byte[] bytes)
+        {
+            HubHostlistDatPayLoad result = new HubHostlistDatPayLoad(bytes);
+
+            return result;
+        }
+
+        public byte[] GetBytes()
+        {
+            List<byte> result = new List<byte>();
+
+            foreach(HubHostInfo i in _hosts)
+            {
+                byte[] tmp = i.Hostname.GetBytes();
+                tmp.Concat(i.Friendlyname.GetBytes()).Concat(i.Description.GetBytes()).Concat(new byte[] { i.RequiresPassword });
+
+                foreach (byte b in tmp)
+                {
+                    result.Add(b);
+                }
+
+            }
+
+            return result.ToArray();
+        }
+    }
+}
---
+++ src/PIZZA.Hub.Core/PayLoads/HubHostlistReqPayLoad.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PIZZA.Hub.Core.PayLoads
+{
+    class HubHostlistReqPayLoad : HubPayLoad
+    {
+        private byte _flags;
+
+
+        private HubHostlistReqPayLoad(byte[] bytes)
+        {
+            _flags = bytes[0];
+        }
+
+        public static HubHostlistReqPayLoad FromBytes(byte[] bytes)
+        {
+            HubHostlistReqPayLoad result = new HubHostlistReqPayLoad(bytes);
+
+            return result;
+        }
+
+        public byte[] GetBytes()
+        {
+            byte[] result = new byte[] { _flags };
+
+            return result;
+        }
+    }
+}
