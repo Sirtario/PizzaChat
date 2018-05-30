c3ff3aee6fda9abcc2de922c1d6ad5531cb194b6
Phillip Eckstein <sirtario9@gmail.com>
2018-05-30 08:28:19 +0200

    add Initial Server methods and csproject

---
+++ src/PizzaChatServer/Server.cs
@@ -0,0 +1,37 @@
+﻿using ACX.ViciOne.TCPLibrary;
+using PIZZA.Chat.Core;
+using System;
+
+namespace PIZZA.Chat.Server
+{
+    public class Server
+    {
+        private ITCPServer _tcpServer;
+
+        public Server()
+        {
+        }
+
+        public Server(ITCPServer server)
+        {
+            _tcpServer = server ?? throw new ArgumentNullException();
+
+            _tcpServer.TCPMessagereceived += _tcpServer_TCPMessagereceived;
+        }
+
+        private void _tcpServer_TCPMessagereceived(object sender, TcpServerMessageReceivedEventArgs e)
+        {
+            var message = PizzaChatMessage.FromBytes(e.Message);
+        }
+
+        public void Start()
+        {
+
+        }
+
+        public void Stop()
+        {
+
+        }
+    }
+}
