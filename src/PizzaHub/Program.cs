1665a2bafcebea4daf908db08f984181916a6ac4
Ronny.Wendler <phracker91@gmail.com>
2018-05-30 14:11:15 +0200

    before Merge

--- src/PizzaHub/Program.cs
+++ src/PizzaHub/Program.cs
@@ -1,21 +1,31 @@
 ﻿using System;
 using System.Threading;
 using PIZZA.Hub.Interface;
+using ACX.ViciOne.TCPLibrary;
+
 
 namespace PIZZA.Hub
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             HubTerminal term = new HubTerminal();
             Thread listener = new Thread(term.RunListener);
+            byte[] Data = new byte[256];
+            TCPServer server = new TCPServer(IsPackageCompleteHb);
 
             term.Cout(ConsoleColor.Gray, "PizzaHub");
 
             term.CommandAdd("test", HubTerminalCommands.testCmd);
             
             listener.Start();
+        }
+
+        private static bool IsPackageCompleteHb(byte[] currentBytes)
+        {
+           
         }
     }
 }
