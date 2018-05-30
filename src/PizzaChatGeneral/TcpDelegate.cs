aac7e77e9aebe78d607e6269779ed6302f39b7aa
Paul.Lißner <paul.lissner@nsc-gmbh.de>
2018-05-30 11:02:46 +0200

    add PizzaChatTcpDelegate

---
+++ src/PizzaChatGeneral/TcpDelegate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PIZZA.Chat.Core
+{
+    public static class TcpDelegate
+    {
+        public static bool IsPIZZAChatMessageComplete(byte[] bytes)
+        {
+            try
+            {
+                var package = PizzaChatMessage.FromBytes(bytes);
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
